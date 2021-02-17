b520e5e5bd6141bdf7bf1a8cd51469b81e6a4ba2
zodiac4006 <278242530@qq.com>
2021-02-17 13:55:04 +0800

    L2D小人资源本地化

---
+++ frontend/static/js/live2d_config.js
@@ -0,0 +1,22 @@
+L2Dwidget.init({
+  "model": {
+    jsonPath: "static/model/shizuku/shizuku.model.json",
+    "scale": 1
+  },
+  "display": {
+    superSample: 1.7, //大小
+    "position": "right",
+    "width": 110,
+    "height": 210,
+    "hOffset": 350,
+    "vOffset": 0
+  },
+  "mobile": {
+    "show": true,
+    "scale": 0.5
+  },
+  "react": {
+    "opacityDefault": 0.8,
+    "opacityOnHover": 0.1
+  }
+});
